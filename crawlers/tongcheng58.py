90cf4e7383b5d78e68f69d1df1bf5da23318d143
xiang <xiangzz159@qq.com>
2019-05-24 13:55:48 +0800

    update 58 code

--- crawlers/tongcheng58.py
+++ crawlers/tongcheng58.py
@@ -24,22 +24,89 @@ import re
 
 
 class tongcheng58(RealEstate):
+    new_house_url = ''
+    second_hand_url = 'https://%s.58.com/ershoufang/'
+    rental_url = 'https://%s.58.com/chuzu/'
 
-    def get_city_all_infos(self, city):
-        url = 'https://%s.58.com/ershoufang/' % city
+    # 新房信息
+    def get_new_hourse_infos(self, city):
+        # 安居客
+        pass
+
+    # 租房信息
+    def get_rental_infos(self, city):
+        url = self.rental_url % (city)
         wb_datas = self.fetch(url)
-        # wb_datas = requests.get(url, headers=config.headers)
+        soup = BeautifulSoup(wb_datas, 'lxml')
+        links = soup.select('ul.house-list > li > div.des > h2 > a')
+        infos = []
+        for i in links:
+            link = i.get('href')
+            if 'e.58.com' in link:
+                continue
+            if link[:2] == '//':
+                link = 'https:' + link
+
+            infos.append(self.get_rental_info(link))
+
+    def get_rental_info(self, link):
+        try:
+            wb_data = self.fetch(link)
+            bs64_str = re.findall("charset=utf-8;base64,(.*?)'\)", wb_data)[0]
+            soup = BeautifulSoup(wb_data, 'lxml')
+
+            title = soup.select('div.house-title > h1')[0].text.strip()
+            title = self.show_real_numb(title, bs64_str)
+
+            pay_way = soup.select('div.house-pay-way > span')
+            price = pay_way[0].text.strip()
+            price = self.show_real_numb(price, bs64_str)
+            deposit_way = pay_way[1].text.strip()
+
+            base_desc = soup.select('ul.f14 > li > span')
+            lease_way = base_desc[1].text.strip()
+            house_type = base_desc[3].text.strip().replace('\n', '').replace(' ', '')
+            house_type = self.show_real_numb(house_type, bs64_str)
+            toward = base_desc[5].text.strip().replace('\n', '').replace(' ', '')
+            toward = self.show_real_numb(toward, bs64_str)
+            address1 = base_desc[7].text.strip()
+            area = base_desc[9].text.strip().replace('\n', '').replace(' ', '')
+            address2 = base_desc[11].text.strip()
+            address = area + '_' + address2 + '_' + address1
+
+            introduce_item = soup.select('ul.introduce-item > li')
+            bright_spot = introduce_item[0].text.strip().replace('\n', '').replace(' ', '')
+            desc = introduce_item[1].text.strip().replace('\n', '').replace(' ', '')
+            return {
+                'title': title,
+                'price': price,
+                'deposit_way': deposit_way,
+                'lease_way': lease_way,
+                'house_type': house_type,
+                'toward': toward,
+                'address': address,
+                'bright_spot': bright_spot,
+                'desc': desc,
+                'link': link
+            }
+
+        except Exception as e:
+            print('数据抓取失败：%s' % str(e))
+
+    # 二手房信息
+    def get_second_hand_infos(self, city):
+        url = self.second_hand_url % city
+        wb_datas = self.fetch(url)
         soup = BeautifulSoup(wb_datas, 'lxml')
         links = soup.select('h2.title > a')
+        infos = []
         for i in links:
             link = i.get('href')
             if link[:2] == '//':
                 link = 'https:' + link
-            print(link)
-            self.get_info(link)
-            print('*' * 50)
+            infos.append(self.get_info(link))
 
-    def get_info(self, link):
+    def get_second_hand_info(self, link):
         try:
             wb_data = self.fetch(link)
             bs64_str = re.findall("charset=utf-8;base64,(.*?)'\)", wb_data)[0]
@@ -47,8 +114,8 @@ class tongcheng58(RealEstate):
             soup = BeautifulSoup(wb_data, 'lxml')
             title = soup.select('div.house-title > h1')[0].text.strip()
             basic_item1 = soup.select('p.house-basic-item1 > span')
-            total_price = self.get_page_show_ret(basic_item1[0].text.strip(), bs64_str)
-            price_per = self.get_page_show_ret(basic_item1[1].text.strip(), bs64_str)
+            total_price = self.show_real_numb(basic_item1[0].text.strip(), bs64_str)
+            price_per = self.show_real_numb(basic_item1[1].text.strip(), bs64_str)
             price_per = price_per.replace('\xa0', ' ')
 
             basic_item2 = soup.select('div.house-basic-item2 > p > span')
@@ -64,7 +131,7 @@ class tongcheng58(RealEstate):
             house_basic1 = basic_item3[1].text.strip().replace('\n', '').replace(' ', '')
             house_basic2 = basic_item3[3].text.strip().replace('\n', '').replace(' ', '')
 
-            data_dict = {
+            return {
                 'title': title,
                 'total_price': total_price,
                 'price_per': price_per,
@@ -75,14 +142,14 @@ class tongcheng58(RealEstate):
                 'toward_main': toward_main,
                 'toward_sub': toward_sub,
                 'house_basic1': house_basic1,
-                'house_basic2:': house_basic2
+                'house_basic2:': house_basic2,
+                'link': link
             }
-            print(data_dict)
 
         except Exception as e:
             print('数据抓取失败：%s' % str(e))
 
-    def get_page_show_ret(self, string, bs64_str):
+    def show_real_numb(self, string, bs64_str):
         font = TTFont(BytesIO(base64.decodebytes(bs64_str.encode())))
         c = font['cmap'].tables[0].ttFont.tables['cmap'].tables[0].cmap
         ret_list = []
@@ -99,6 +166,6 @@ class tongcheng58(RealEstate):
             ret_str_show += str(num)
         return ret_str_show
 
-
-f = tongcheng58()
-f.get_city_all_infos('sz')
+# f = tongcheng58()
+# infos = f.get_rental_infos('sz')
+# print(infos)
